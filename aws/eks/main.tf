c73e3b1cb69ea95459a89e6ff404bed15c5c354f
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2021-07-25 23:58:25 +0300

    Upgrade hashicorp/kubernetes to 2.3.2
    2.2.0: https://github.com/hashicorp/terraform-provider-kubernetes/releases/tag/v2.2.0
    2.3.0: https://github.com/hashicorp/terraform-provider-kubernetes/releases/tag/v2.3.0
    2.3.1: https://github.com/hashicorp/terraform-provider-kubernetes/releases/tag/v2.3.1
    2.3.2: https://github.com/hashicorp/terraform-provider-kubernetes/releases/tag/v2.3.2

--- aws/eks/main.tf
+++ aws/eks/main.tf
@@ -7,7 +7,7 @@ terraform {
 
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = "~> 2.1"
+      version = "~> 2.3.2"
     }
   }
 }
--- k8s/basic/main.tf
+++ k8s/basic/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = "~> 2.1"
+      version = "~> 2.3.2"
     }
 
     kubernetes-alpha = {
