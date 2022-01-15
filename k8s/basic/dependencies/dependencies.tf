ab72625450aab70784bd9e173f2d9bf21bb63be4
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2022-01-15 17:37:25 +0100

    Upgrade cert-manager 1.4.1 -> 1.6.1
    Changelog:
    * 1.5.0: https://github.com/jetstack/cert-manager/releases/tag/v1.5.0
    * 1.6.0: https://github.com/jetstack/cert-manager/releases/tag/v1.6.0
    * 1.6.1: https://github.com/jetstack/cert-manager/releases/tag/v1.6.1

--- k8s/basic/dependencies/dependencies.tf
+++ k8s/basic/dependencies/dependencies.tf
@@ -16,7 +16,7 @@ resource "kubernetes_namespace" "cert-manager" {
 resource "helm_release" "cert-manager" {
   name       = "cert-manager"
   chart      = "cert-manager"
-  version    = "1.4.1"
+  version    = "1.6.1"
   repository = "https://charts.jetstack.io"
   namespace  = kubernetes_namespace.cert-manager.id
 
