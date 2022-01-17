52791dd9833bce69984e5fcc43819109c4ef730b
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2022-01-17 20:02:28 +0100

    Make aws/redis module to setup necessary security group rules
    Since aws/eks module no longer creates broad security group rules we
    have to add necessary groups on our own.

--- aws/redis/redis.tf
+++ aws/redis/redis.tf
@@ -19,12 +19,12 @@ variable "vpc_id" {
   type = string
 }
 
-variable "eks_private_subnets_cidr_blocks" {
-  type = set(string)
-}
-
 variable "redis_subnets" {
   type = map(string)
+}
+
+variable "allow_security_group_ids" {
+  type = list(string)
 }
 
 resource "aws_subnet" "redis" {
@@ -42,25 +42,13 @@ resource "aws_elasticache_subnet_group" "redis" {
 }
 
 resource "aws_security_group" "redis" {
-  name = "${var.app}-${var.environment}-redis"
-  description = "Allows access from EKS private subnets to Redis"
+  name = "${var.app}-${var.environment}-elasticache-redis"
+  description = "Redis security group"
 
   vpc_id = var.vpc_id
 
-  ingress {
-    description = "From EKS private subnets to Redis"
-    from_port = 6379
-    to_port = 6379
-    protocol = "tcp"
-    cidr_blocks = var.eks_private_subnets_cidr_blocks
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -82,10 +70,62 @@ resource "aws_elasticache_cluster" "redis" {
   subnet_group_name = aws_elasticache_subnet_group.redis.name
 }
 
+resource "aws_security_group_rule" "redis_ingress" {
+  for_each = toset(var.allow_security_group_ids)
+
+  security_group_id = aws_security_group.redis.id
+  description = "Ingress to redis"
+  type = "ingress"
+  protocol = "tcp"
+  from_port = aws_elasticache_cluster.redis.cache_nodes.0.port
+  to_port = aws_elasticache_cluster.redis.cache_nodes.0.port
+  source_security_group_id = each.value
+}
+
+resource "aws_security_group_rule" "redis_egress" {
+  for_each = toset(var.allow_security_group_ids)
+
+  security_group_id = aws_security_group.redis.id
+  description = "Egress from redis"
+  type = "egress"
+  protocol = "tcp"
+  from_port = 0
+  to_port = 0
+  source_security_group_id = each.value
+}
+
+resource "aws_security_group_rule" "egress_to_redis" {
+  for_each = toset(var.allow_security_group_ids)
+
+  security_group_id = each.value
+  description = "Egress to redis"
+  type = "egress"
+  protocol = "tcp"
+  from_port = aws_elasticache_cluster.redis.cache_nodes.0.port
+  to_port = aws_elasticache_cluster.redis.cache_nodes.0.port
+  source_security_group_id = aws_security_group.redis.id
+}
+
+resource "aws_security_group_rule" "ingress_from_redis" {
+  for_each = toset(var.allow_security_group_ids)
+
+  security_group_id = each.value
+  description = "Ingress from redis"
+  type = "ingress"
+  protocol = "tcp"
+  from_port = 0
+  to_port = 0
+  source_security_group_id = aws_security_group.redis.id
+}
+
 output "host" {
   value = aws_elasticache_cluster.redis.cache_nodes.0.address
 }
 
 output "port" {
   value = aws_elasticache_cluster.redis.cache_nodes.0.port
+}
+
+output "security_group_id" {
+  value = aws_security_group.redis.id
 }
