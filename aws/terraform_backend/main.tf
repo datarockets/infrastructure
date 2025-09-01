495270aee9daa551ca0d42c4d3ef7749df534149
Sofia <misofser@gmail.com>
2025-09-01 13:19:32 +0400

    Integrate Trivy

--- aws/terraform_backend/main.tf
+++ aws/terraform_backend/main.tf
@@ -1,3 +1,8 @@
+# Ignored rules:
+#   Bucket does not have encryption enabled
+#   Bucket has logging disabled
+#   Bucket does not encrypt data with a customer managed key
+#trivy:ignore:AVD-AWS-0089 trivy:ignore:AVD-AWS-0132 trivy:ignore:AVD-AWS-0088
 resource "aws_s3_bucket" "state" {
   bucket = var.s3_bucket
 }
@@ -10,6 +15,10 @@ resource "aws_s3_bucket_versioning" "state" {
   }
 }
 
+# Ignored rules:
+#   Point-in-time recovery is not enabled
+#   Table encryption does not use a customer-managed KMS key
+#trivy:ignore:AVD-AWS-0024 trivy:ignore:AVD-AWS-0025
 resource "aws_dynamodb_table" "state" {
   name         = var.dynamodb_table
   billing_mode = "PAY_PER_REQUEST"
@@ -19,4 +28,13 @@ resource "aws_dynamodb_table" "state" {
     name = "LockID"
     type = "S"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "state" {
+  bucket = aws_s3_bucket.state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
